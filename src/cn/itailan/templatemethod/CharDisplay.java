3da71e9c975a9229d5e4f5c0aa223f4a311d111c
lan <gui_052@163.com>
2020-03-21 13:57:47 +0800

    feat:模板方法模式

---
+++ src/cn/itailan/templatemethod/CharDisplay.java
@@ -0,0 +1,25 @@
+package cn.itailan.templatemethod;
+
+public class CharDisplay extends AbstractDisplay {
+
+    private char aChar;
+
+    public CharDisplay(char aChar) {
+        this.aChar = aChar;
+    }
+
+    @Override
+    public void open() {
+        System.out.println("<<");
+    }
+
+    @Override
+    public void print() {
+        System.out.println(aChar);
+    }
+
+    @Override
+    public void close() {
+        System.out.println(">>");
+    }
+}
