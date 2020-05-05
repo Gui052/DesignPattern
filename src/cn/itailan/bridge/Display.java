7dbc1a1c7499611c59cad7bb65cc3301951e373c
lan <gui_052@163.com>
2020-05-05 11:30:37 +0800

    feat:桥接模式

---
+++ src/cn/itailan/bridge/Display.java
@@ -0,0 +1,30 @@
+package cn.itailan.bridge;
+
+/**
+ * 类的功能层次结构
+ */
+public class Display {
+    //委托,Display交予DisplayImpl具体实现工作
+    private DisplayImpl display;
+
+    public Display(DisplayImpl display) {
+        this.display = display;
+    }
+    public void open() {
+        display.rawOpen();
+    }
+
+    public void print() {
+        display.rawPrint();
+    }
+
+    public void close() {
+        display.rawClose();
+    }
+
+    public final void display() {
+        open();
+        print();
+        close();
+    }
+}
---
+++ src/cn/itailan/bridge/Main.java
@@ -0,0 +1,13 @@
+package cn.itailan.bridge;
+
+public class Main {
+    public static void main(String[] args) {
+        Display d1 = new Display(new StringDisplayImpl("hello"));
+        Display d2 = new CountDisplay(new StringDisplayImpl("hello"));
+        CountDisplay d3 = new CountDisplay(new StringDisplayImpl("hello,hello"));
+        d1.display();
+        d2.display();
+        d3.display();
+        d3.multiDisplay(5);
+    }
+}
