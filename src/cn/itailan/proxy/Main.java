ee27848f148a33428d3adfcbe1c4ea68b164c3f0
lan <gui_052@163.com>
2020-06-17 00:09:51 +0800

    feat:代理模式

---
+++ src/cn/itailan/proxy/Main.java
@@ -0,0 +1,16 @@
+package cn.itailan.proxy;
+
+/**
+ * @author Mr.Lan
+ * @version 1.0.0
+ * @since 2020/6/16 11:53 下午
+ */
+public class Main {
+    public static void main(String[] args) {
+        Printable printable = new PrinterProxy("Alice");
+        System.out.println("现在的名字是" + printable.getPrinterName());
+        printable.setPrinterName("Bob");
+        System.out.println("现在的名字是" + printable.getPrinterName());
+        printable.print("Hello Word");
+    }
+}
